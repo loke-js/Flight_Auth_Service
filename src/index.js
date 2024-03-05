7a5cbacea614941bd4e1c8f97aa719e4dbd712ac
lokendra22844 <lokendratiwari22844@gmail.com>
2024-03-05 23:20:45 +0530

    First commit:project skeleton setup done

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app =express();
+const {PORT}=require('./config/serverConfig');
+const prepareAndStartServer=()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server started on port: ${PORT}`)
+    })}
+
+prepareAndStartServer();
