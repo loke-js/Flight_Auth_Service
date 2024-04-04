dc611cc3d102cb31148211c50cf5891d443c70df
lokendra22844 <lokendratiwari22844@gmail.com>
2024-04-05 01:01:01 +0530

    Added roles and user_roles through tables

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser');
 const {PORT}=require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-const {User}= require('./models/index');
+const {User,Role}= require('./models/index');
 const bcrypt=require('bcrypt');
 
 const UserService = require('./services/user-service');
-
+const db = require('./models/index');
 const app =express();
 
 const prepareAndStartServer=()=>{
@@ -19,7 +19,14 @@ const prepareAndStartServer=()=>{
 
     app.listen(PORT, async()=>{
         console.log(`Server started on port: ${PORT}`);
-        
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true});  
+        }
+        const u1= await User.findByPk(1);
+        const r1= await Role.findByPk(1);
+        // u1.addRole(r1);
+        const response = await u1.hasRole(r1);
+        console.log(response);
         const service = new UserService();
         // const newToken = service.createToken({email:'hulk@admin.com',id:1});
         // console.log(newToken);
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -41,7 +41,7 @@ class UserService{
             if(!response){
                 throw {error:'Invalid Token'};
             }
-            const user = this.userRepository.getById(response.id);
+            const user = await this.userRepository.getById(response.id);
             if(!user){
                 throw {error:'User not found'};
             }
